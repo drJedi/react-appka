a60ccdac807c5c908c11da45d11d902a2af0ca7a
Pawel <pawe.wilkosz@gmail.com>
2022-03-23 10:37:10 +0100

    Updates

---
+++ precache-manifest.c2e8b2b51325faf9b3b7dc446303f9da.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d60d26ad0c3618d2e82",
+    "url": "/react-appka/static/css/main.f8029b60.chunk.css"
+  },
+  {
+    "revision": "7d60d26ad0c3618d2e82",
+    "url": "/react-appka/static/js/main.f2e3042c.chunk.js"
+  },
+  {
+    "revision": "d0e00ea07e8051a643e3",
+    "url": "/react-appka/static/js/runtime~main.8c148fce.js"
+  },
+  {
+    "revision": "d14e2ba4e3594e20c2c1",
+    "url": "/react-appka/static/js/2.c35a0f9c.chunk.js"
+  },
+  {
+    "revision": "70431f78f7996a939578e28a03bfc84e",
+    "url": "/react-appka/static/media/img11.70431f78.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-appka/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-appka/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-appka/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "083147fdba08fd334a1a9e109d931c44",
+    "url": "/react-appka/index.html"
+  }
+];
